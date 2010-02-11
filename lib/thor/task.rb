37e8a9bda1c6da7477b6d407a69e250b48ee2696
Ryan Heimbuch <rheimbuch@gmail.com>
2010-02-10 18:12:39 -0700

    Revert "Require 'thor/group' so that task.rb line 92 doesn't blow up."
    Patches the problem at task.rb:92, but spec_helper.rb blows up when loading fixtures/group.thor
    
    This reverts commit 5943a1185a346317c9133a7566a2b53268057aec.

--- lib/thor/task.rb
+++ lib/thor/task.rb
@@ -1,5 +1,3 @@
-require 'thor/group'
-
 class Thor
   class Task < Struct.new(:name, :description, :usage, :options)
     FILE_REGEXP = /^#{Regexp.escape(File.dirname(__FILE__))}/
@@ -40,7 +38,7 @@ class Thor
       parse_no_method_error(instance, e)
     end
 
-    # Returns the formatted usage by injecting given required arguments
+    # Returns the formatted usage by injecting given required arguments 
     # and required options into the given usage.
     def formatted_usage(klass, namespace=true)
       namespace = klass.namespace unless namespace == false
