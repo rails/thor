86e113954010e4972f45b29366ba30a5cb5a8087
Erik Michaels-Ober <sferik@gmail.com>
2010-11-20 07:43:52 -0800

    Make CreateLink inherit from CreateFile and update documentation

--- lib/thor/actions/create_file.rb
+++ lib/thor/actions/create_file.rb
@@ -1,5 +1,4 @@
 require 'thor/actions/empty_directory'
-require 'thor/actions/creation_utils'
 
 class Thor
   module Actions
@@ -28,7 +27,7 @@ class Thor
     end
     alias :add_file :create_file
 
-    # AddFile is a subset of Template, which instead of rendering a file with
+    # CreateFile is a subset of Template, which instead of rendering a file with
     # ERB, it gets the content from the user.
     #
     class CreateFile < EmptyDirectory #:nodoc:
@@ -48,6 +47,16 @@ class Thor
         exists? && File.binread(destination) == render
       end
 
+      # Holds the content to be added to the file.
+      #
+      def render
+        @render ||= if data.is_a?(Proc)
+          data.call
+        else
+          data
+        end
+      end
+
       def invoke!
         invoke_with_conflict_check do
           FileUtils.mkdir_p(File.dirname(destination))
@@ -56,7 +65,40 @@ class Thor
         given_destination
       end
 
-      include CreationUtils
+      protected
+
+        # Now on conflict we check if the file is identical or not.
+        #
+        def on_conflict_behavior(&block)
+          if identical?
+            say_status :identical, :blue
+          else
+            options = base.options.merge(config)
+            force_or_skip_or_conflict(options[:force], options[:skip], &block)
+          end
+        end
+
+        # If force is true, run the action, otherwise check if it's not being
+        # skipped. If both are false, show the file_collision menu, if the menu
+        # returns true, force it, otherwise skip.
+        #
+        def force_or_skip_or_conflict(force, skip, &block)
+          if force
+            say_status :force, :yellow
+            block.call unless pretend?
+          elsif skip
+            say_status :skip, :yellow
+          else
+            say_status :conflict, :red
+            force_or_skip_or_conflict(force_on_collision?, true, &block)
+          end
+        end
+
+        # Shows the file collision menu to the user and gets the result.
+        #
+        def force_on_collision?
+          base.shell.file_collision(destination){ render }
+        end
 
     end
   end
--- lib/thor/actions/create_link.rb
+++ lib/thor/actions/create_link.rb
@@ -1,16 +1,15 @@
 require 'thor/actions/empty_directory'
-require 'thor/actions/creation_utils'
 
 class Thor
   module Actions
 
-    # Create a new file relative to the destination root with the given data,
-    # which is the return value of a block or a data string.
+    # Create a new file relative to the destination root from the given source.
     #
     # ==== Parameters
     # destination<String>:: the relative path to the destination root.
-    # data<String|NilClass>:: the data to append to the file.
+    # source<String|NilClass>:: the relative path to the source root.
     # config<Hash>:: give :verbose => false to not log the status.
+    #   :: give :symbolic => false for hard link.
     #
     # ==== Examples
     #
@@ -23,16 +22,11 @@ class Thor
     end
     alias :add_link :create_link
 
-    # AddFile is a subset of Template, which instead of rendering a file with
-    # ERB, it gets the content from the user.
+    # CreateLink is a subset of CreateFile, which instead of taking a block of
+    # data, just takes a source string from the user.
     #
-    class CreateLink < EmptyDirectory #:nodoc:
+    class CreateLink < CreateFile #:nodoc:
       attr_reader :data
-
-      def initialize(base, destination, data, config={})
-        @data = data
-        super(base, destination, config)
-      end
 
       # Checks if the content of the file at the destination is identical to the rendered result.
       #
@@ -57,8 +51,6 @@ class Thor
         end
         given_destination
       end
-
-      include CreationUtils
 
     end
   end
