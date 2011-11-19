97584e2ba1089c54638d990af45e731f90993e19
mpapis <mpapis@gmail.com>
2011-11-19 17:16:22 +0100

    allow recognizing symlinked ruby as ruby_command

--- lib/thor/util.rb
+++ lib/thor/util.rb
@@ -216,8 +216,27 @@ class Thor
     #
     def self.ruby_command
       @ruby_command ||= begin
-        ruby = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
+        ruby_name = RbConfig::CONFIG['ruby_install_name']
+        ruby = File.join(RbConfig::CONFIG['bindir'], ruby_name)
         ruby << RbConfig::CONFIG['EXEEXT']
+
+        # avoid using different name then ruby (on platforms supporting links)
+        if ruby_name != 'ruby' && File.respond_to?(:readlink)
+          begin
+            alternate_ruby = File.join(RbConfig::CONFIG['bindir'], 'ruby')
+            alternate_ruby << RbConfig::CONFIG['EXEEXT']
+  
+            # ruby is a symlink
+            if File.symlink? alternate_ruby
+              linked_ruby = File.readlink alternate_ruby
+  
+              # symlink points to 'ruby_install_name'
+              ruby = alternate_ruby if linked_ruby == ruby_name || linked_ruby == ruby
+            end
+          rescue NotImplementedError
+            # just ignore on windows
+          end
+        end
 
         # escape string in case path to ruby executable contain spaces.
         ruby.sub!(/.*\s.*/m, '"\&"')
