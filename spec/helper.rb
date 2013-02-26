34c2440355be7a29b28ea69ccb8f3eba20f4d7d9
Erik Michaels-Ober <sferik@gmail.com>
2013-02-26 10:37:54 -0800

    Fix indentation [ci skip]

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,8 +4,8 @@ require 'simplecov'
 require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
 ]
 SimpleCov.start
 
