e95dc869e909f7bb39070ff24ccfca347b79bf5f
Vipul A M <vipulnsward@gmail.com>
2013-04-04 19:44:37 +0530

    fix private attribute warning

--- lib/thor/actions/file_manipulation.rb
+++ lib/thor/actions/file_manipulation.rb
@@ -293,8 +293,11 @@ class Thor
     end
     alias :remove_dir :remove_file
 
-  private
     attr_accessor :output_buffer
+    private :output_buffer,  :output_buffer=
+
+    private
+
     def concat(string)
       @output_buffer.concat(string)
     end
