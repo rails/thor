82f098b5f1bc1fdd098f3455dcad7d0f3ba628a4
Max Meyer <dev@fedux.org>
2013-07-01 16:34:46 +0200

    make the current command chain accessible in command

--- spec/invocation_spec.rb
+++ spec/invocation_spec.rb
@@ -58,7 +58,12 @@ describe Thor::Invocation do
       expect(A.new([], :defaulted_value => 'not default').invoke('b:four')).to eq('not default')
     end
 
-    it 'dump configuration values to be used in the invoked class' do
+    it "returns the command chain" do
+      expect(I.new.invoke("two")).to eq([ :two ])
+      expect(J.start(["one", "two" ])).to eq([ :one, :two ])
+    end
+
+    it "dump configuration values to be used in the invoked class" do
       base = A.new
       expect(base.invoke('b:three').shell).to eq(base.shell)
     end
