481e62401eab48732dd4a6dd2891909721dc0569
George Brocklehurst <george.brocklehurst@gmail.com>
2013-08-11 23:14:34 +0200

    Support Readline tab completion.
    When the input is restricted by the :limited_to option, support tab
    completion for the options.

--- lib/thor/line_editor/readline.rb
+++ lib/thor/line_editor/readline.rb
@@ -8,6 +8,7 @@ class Thor
       end
 
       def readline
+        ::Readline.completion_proc = completion_proc
         ::Readline.readline(prompt, add_to_history?)
       end
 
@@ -15,6 +16,18 @@ class Thor
 
       def add_to_history?
         options.fetch(:add_to_history, true)
+      end
+
+      def completion_proc
+        if completion_options.any?
+          Proc.new do |text|
+            completion_options.select { |option| option.start_with?(text) }
+          end
+        end
+      end
+
+      def completion_options
+        options.fetch(:limited_to, [])
       end
     end
   end
--- spec/line_editor/readline_spec.rb
+++ spec/line_editor/readline_spec.rb
@@ -22,14 +22,28 @@ describe Thor::LineEditor::Readline do
   describe '#readline' do
     it 'invokes the readline library' do
       expect(::Readline).to receive(:readline).with('> ', true).and_return('foo')
+      expect(::Readline).to receive(:completion_proc=).with(nil)
       editor = Thor::LineEditor::Readline.new('> ', {})
       expect(editor.readline).to eq('foo')
     end
 
     it 'supports the add_to_history option' do
       expect(::Readline).to receive(:readline).with('> ', false).and_return('foo')
+      expect(::Readline).to receive(:completion_proc=).with(nil)
       editor = Thor::LineEditor::Readline.new('> ', :add_to_history => false)
       expect(editor.readline).to eq('foo')
+    end
+
+    it 'provides tab completion when given a limited_to option' do
+      expect(::Readline).to receive(:readline)
+      expect(::Readline).to receive(:completion_proc=) do |proc|
+        expect(proc.call('')).to eq ['Apples', 'Chicken', 'Chocolate']
+        expect(proc.call('Ch')).to eq ['Chicken', 'Chocolate']
+        expect(proc.call('Chi')).to eq ['Chicken']
+      end
+
+      editor = Thor::LineEditor::Readline.new('Best food: ', :limited_to => ['Apples', 'Chicken', 'Chocolate'])
+      editor.readline
     end
   end
 end
