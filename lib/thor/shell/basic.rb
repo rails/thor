7e5b2bf4a197272d8b109fbda5a399425c9be95e
Karim AININE <karim.ainine@gmail.com>
2014-08-23 22:23:31 +1000

    ask_filtered should allow users a skip option if they do not want to select one of the :limited options

--- lib/thor/shell/basic.rb
+++ lib/thor/shell/basic.rb
@@ -408,10 +408,14 @@ class Thor
       def ask_filtered(statement, color, options)
         answer_set = options[:limited_to]
         correct_answer = nil
+        if options.has_key? :skip
+          statement += options[:skip].strip.empty? ? " (Enter to skip)" : " (#{options[:skip]} to skip)"
+        end
         until correct_answer
           answers = answer_set.join(", ")
           answer = ask_simply("#{statement} [#{answers}]", color, options)
-          correct_answer = answer_set.include?(answer) ? answer : nil
+          skipped = (options.has_key?(:skip) && (answer == options[:skip].chomp))
+          correct_answer = answer_set.include?(answer) || skipped ? answer : nil
           say("Your response must be one of: [#{answers}]. Please try again.") unless correct_answer
         end
         correct_answer
