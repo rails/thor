30a0ab0c6c690acc6532237fe4f46614178ceabf
Justin Campbell <justin@justincampbell.me>
2015-01-20 22:59:12 -0500

    Add #fetch to HashWithIndifferentAccess

--- lib/thor/core_ext/hash_with_indifferent_access.rb
+++ lib/thor/core_ext/hash_with_indifferent_access.rb
@@ -28,6 +28,10 @@ class Thor
         super(convert_key(key))
       end
 
+      def fetch(key, *args)
+        super(convert_key(key), *args)
+      end
+
       def values_at(*indices)
         indices.map { |key| self[convert_key(key)] }
       end
