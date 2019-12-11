8e296f6a4e8333d3bcc8c4473575a53d57af76d0
Tom Finill <tom@kalohq.com>
2019-12-11 18:00:58 +0100

    Allow for nested `no_commands` blocks.
    In certain circumstances we may wish to nest `no_commands` blocks.
    For example, if we want to import any module which includes both methods
    and `attr_reader` invocations.
    
    However, since when exiting the no_commands block the @no_commands
    instance var is reset to `false` we get unexpected behaviour.
    
    This change uses a `NestedContext` object to track the depth of the
    no_command blocks and ensure that we only start creating commands again
    once we've left all of them.

---
+++ spec/nested_context_spec.rb
@@ -0,0 +1,20 @@
+require "helper"
+
+describe Thor::NestedContext do
+  subject(:context) { described_class.new }
+  
+  describe "#enter" do
+    it "is never empty within the entered block" do
+      context.enter do
+        context.enter {}
+
+        expect(context).to be_entered
+      end
+    end
+
+    it "is empty when outside of all blocks" do
+      context.enter { context.enter {} }
+      expect(context).not_to be_entered
+    end
+  end
+end
