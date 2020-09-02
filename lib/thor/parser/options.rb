214acdb50c5d845328cf0ab8d7a9d9187002baf3
Jack Zhou <univerio@gmail.com>
2020-09-01 17:45:37 -0700

    Allow leading hyphen in switch values when specified with =

--- lib/thor/parser/options.rb
+++ lib/thor/parser/options.rb
@@ -45,6 +45,7 @@ class Thor
       @switches = {}
       @extra = []
       @stopped_parsing_after_extra_index = nil
+      @force_current_to_be_value = false
 
       options.each do |option|
         @switches[option.switch_name] = option
@@ -74,8 +75,19 @@ class Thor
       end
     end
 
+    def shift
+      @force_current_to_be_value = false
+      super
+    end
+
+    def unshift(arg, force_value: false)
+      @force_current_to_be_value = force_value
+      super(arg)
+    end
+
     def parse(args) # rubocop:disable MethodLength
       @pile = args.dup
+      @force_current_to_be_value = false
       @parsing_options = true
 
       while peek
@@ -88,7 +100,10 @@ class Thor
             when SHORT_SQ_RE
               unshift($1.split("").map { |f| "-#{f}" })
               next
-            when EQ_RE, SHORT_NUM
+            when EQ_RE
+              unshift($2, force_value: true)
+              switch = $1
+            when SHORT_NUM
               unshift($2)
               switch = $1
             when LONG_RE, SHORT_RE
@@ -148,6 +163,7 @@ class Thor
     # Two booleans are returned.  The first is true if the current value
     # starts with a hyphen; the second is true if it is a registered switch.
     def current_is_switch?
+      return [false, false] if @force_current_to_be_value
       case peek
       when LONG_RE, SHORT_RE, EQ_RE, SHORT_NUM
         [true, switch?($1)]
@@ -159,6 +175,7 @@ class Thor
     end
 
     def current_is_switch_formatted?
+      return false if @force_current_to_be_value
       case peek
       when LONG_RE, SHORT_RE, EQ_RE, SHORT_NUM, SHORT_SQ_RE
         true
@@ -168,6 +185,7 @@ class Thor
     end
 
     def current_is_value?
+      return true if @force_current_to_be_value
       peek && (!parsing_options? || super)
     end
 
