aab2c57d1af05884947f83c7822c1d586a1d18fa
Petrik <petrik@deheus.net>
2023-08-06 18:51:59 +0200

    Add support for printing tables with borders
    Adding borders to tables can improve their legibility.
    
    This would allow replacing the custom table for code statistics in
    Rails, with the generic implementation in Thor.
    
    By adding :separators to a table a horizontal separator will be added.
    This functionality was inspired by: https://github.com/piotrmurach/tty-table

--- lib/thor/shell/table_printer.rb
+++ lib/thor/shell/table_printer.rb
@@ -4,12 +4,15 @@ require_relative "terminal"
 class Thor
   module Shell
     class TablePrinter < ColumnPrinter
+      BORDER_SEPARATOR = :separator
+
       def initialize(stdout, options = {})
         super
         @formats = []
         @maximas = []
         @colwidth = options[:colwidth]
         @truncate = options[:truncate] == true ? Terminal.terminal_width : options[:truncate]
+        @padding = 1
       end
 
       def print(array)
@@ -17,33 +20,33 @@ class Thor
 
         prepare(array)
 
+        print_border_separator if options[:borders]
+
         array.each do |row|
+          if options[:borders] && row == BORDER_SEPARATOR
+            print_border_separator
+            next
+          end
+
           sentence = "".dup
 
           row.each_with_index do |column, index|
-            maxima = @maximas[index]
-
-            f = if column.is_a?(Numeric)
-              if index == row.size - 1
-                # Don't output 2 trailing spaces when printing the last column
-                "%#{maxima}s"
-              else
-                "%#{maxima}s  "
-              end
-            else
-              @formats[index]
-            end
-            sentence << f % column.to_s
+            sentence << format_cell(column, row.size, index)
           end
 
           sentence = truncate(sentence)
+          sentence << "|" if options[:borders]
           stdout.puts sentence
+
         end
+        print_border_separator if options[:borders]
       end
 
     private
 
       def prepare(array)
+        array = array.reject{|row| row == BORDER_SEPARATOR }
+
         @formats << "%-#{@colwidth + 2}s".dup if @colwidth
         start = @colwidth ? 1 : 0
 
@@ -51,8 +54,11 @@ class Thor
 
         start.upto(colcount - 1) do |index|
           maxima = array.map { |row| row[index] ? row[index].to_s.size : 0 }.max
+
           @maximas << maxima
-          @formats << if index == colcount - 1
+          @formats << if options[:borders]
+             "%-#{maxima}s".dup
+          elsif index == colcount - 1
             # Don't output 2 trailing spaces when printing the last column
             "%-s".dup
           else
@@ -62,6 +68,37 @@ class Thor
 
         @formats[0] = @formats[0].insert(0, " " * @indent)
         @formats << "%s"
+      end
+
+      def format_cell(column, row_size, index)
+        maxima = @maximas[index]
+
+        f = if column.is_a?(Numeric)
+          if options[:borders]
+            # With borders we handle padding separately
+            "%#{maxima}s"
+          elsif index == row_size - 1
+            # Don't output 2 trailing spaces when printing the last column
+            "%#{maxima}s"
+          else
+            "%#{maxima}s  "
+          end
+        else
+          @formats[index]
+        end
+
+        cell = "".dup
+        cell << "|" + " " * @padding if options[:borders]
+        cell << f % column.to_s
+        cell << " " * @padding if options[:borders]
+        cell
+      end
+
+      def print_border_separator
+        top = @maximas.map do |maxima|
+          " " * @indent + "+" + "-" * (maxima + 2 * @padding)
+        end
+        stdout.puts top.join + "+"
       end
 
       def truncate(string)
